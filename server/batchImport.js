bf3e52e83623f5a314e922c75fc98484f0a5ebe6
Ilgiz Temi <temirbekov91@gmail.com>
2022-08-06 17:48:33 -0400

    initial set up done

---
+++ server/batchImport.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+const adds = require("./data/data.json");
+console.log(data)
+const uploadAdds = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db("real-estate");
+    await db.collection("adds").insertMany(adds);
+  } catch (error) {
+    console.log(error);
+  }
+  client.close();
+};
+// uploadAdds();
